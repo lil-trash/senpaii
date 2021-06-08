2c95fd377b27a01660b41e24d8bf20be251fe62f
lil-trash <73224935+lil-trash@users.noreply.github.com>
2021-06-08 21:24:01 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'senpaii',        
+  packages = ['senpaii'],  
+  version = '1',      
+  license='MIT',        
+  description = """Senpaii's thighs""",  
+  author = 'LilPork',                  
+  author_email = 'defentlymyrealemail@gmail.com',    
+  url = 'https://github.com/lil-trash/senpaii',  
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['thighs', 'senpaii', 'cum'],   
+  install_requires=[           
+          'PIL',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
